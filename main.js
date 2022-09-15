8469cfe3af22ebb2e0eb1202ec3a330b6f96b74e
atheeyaparveen <atheeyacse@gmail.com>
2022-09-15 05:39:38 +0530

    magic eight ball commit

---
+++ main.js
@@ -0,0 +1,93 @@
+// magic eight ball
+
+const userName="jose";
+ userName ? console.log(`hello ,${userName}!`) : console.log(`hello!`);
+let userQuestion=" how are you   ?";
+console.log(`${userName} asked: ${userQuestion}`);
+let randomNumber = Math.floor(Math.random()*8);
+let eightBall ="";
+
+/*
+if(randomNumber===0){
+   eightBall="It is certain"
+} else if(randomNumber===1){
+eightBall= "It is decidedly so";
+} else if(randomNumber===2){
+  eightBall= "Reply hazy try again";
+} else if(randomNumber===3){
+  eightBall= "Cannot predict now";
+} else if(randomNumber===4){
+   eightBall=  "Do not count on it";
+} else if(randomNumber===5){
+   eightBall= "My sources say no";
+} else if(randomNumber===6){
+ eightBall=  "Outlook not so good";
+} else if(randomNumber===7){
+  eightBall=  "Signs point to yes";
+}else {
+    console.log(`something is broken `);
+}*/
+
+switch(randomNumber){
+    case 0: eightBall="It is certain";
+    break;
+    case 1: eightBall= "It is decidedly so";
+    break;
+    case 2: eightBall= "Reply hazy try again";
+    break;
+    case 3:  eightBall= "Cannot predict now";
+    break;
+    case 4:  eightBall=  "Do not count on it";
+    break;
+    case 5: eightBall= "My sources say no";
+    break;
+    case 6: eightBall=  "Outlook not so good";
+    break;
+    case 7:eightBall=  "Signs point to yes";
+    break;
+    default:  console.log(`something is broken `);
+    break;
+}
+
+console.log ( eightBall);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
